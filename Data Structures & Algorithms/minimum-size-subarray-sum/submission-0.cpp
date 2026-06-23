bf53f38323a6b8f3d25a6fc8fcf3d7e42552dc4a
Joohhnnyyy <johnsonansh32@gmail.com>
2026-06-23 14:48:07 +0530

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0 ; 
+        int res = INT_MAX;
+        int sum = 0;
+        for(int right = 0 ;right < nums.size() ; right++){
+            sum += nums[right];
+            while(sum >= target ){
+                int len = right - left + 1;
+                res = min(res , len);
+                sum -= nums[left++];
+            }
+        }return (res == INT_MAX ? 0 : res);
+    }
+};
